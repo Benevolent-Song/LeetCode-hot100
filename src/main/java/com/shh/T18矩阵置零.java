6f9fdd4952a3f14eb47309ac790b495a5b69e352
Benevolent-Song <1229143600@qq.com>
2023-11-12 14:36:35 +0800

    Initial commit

---
+++ src/main/java/com/shh/T18矩阵置零.java
@@ -0,0 +1,12 @@
+//给定一个 m x n 的矩阵，如果一个元素为 0 ，则将其所在行和列的所有元素都设为 0 。请使用 原地 算法。
+package com.shh;
+public class T18矩阵置零 {
+    public static void main(String[] args) {
+
+    }
+}
+class Solution18 {
+    public void setZeroes(int[][] matrix) {
+
+    }
+}
