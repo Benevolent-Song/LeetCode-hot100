da31a014ed134c67c889ab2267795b1d30b83b9d
Benevolent-Song <1229143600@qq.com>
2023-11-12 16:06:52 +0800

    add1

--- src/main/java/com/shh/T19螺旋矩阵.java
+++ src/main/java/com/shh/T19螺旋矩阵.java
@@ -1,4 +1,16 @@
 package com.shh;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class T19螺旋矩阵 {
+    public static void main(String[] args) {
+
+    }
+}
+class Solution19{
+    public List<Integer> spiralOrder(int[][] matrix) {
+       List <Integer> list=new ArrayList<>();
+       return list;
+    }
 }
